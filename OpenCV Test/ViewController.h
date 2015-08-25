603f2f7945f84308437f8332f0fb7a305a930c64
Stuart Farmer <email@stuartfarmer.me>
2015-08-25 16:14:52 -0400

    first commit

---
+++ OpenCV Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCV Test
+//
+//  Created by Stuart Farmer on 8/25/15.
+//  Copyright (c) 2015 Stuart Farmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenCV Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OpenCV Test
+//
+//  Created by Stuart Farmer on 8/25/15.
+//  Copyright (c) 2015 Stuart Farmer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
